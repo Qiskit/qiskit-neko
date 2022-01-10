d64d41254d91899ab85b56595a3be6374eb26cda
Matthew Treinish <mtreinish@kortar.org>
2022-01-10 11:03:09 -0500

    Add tests based on circuit basics tutorial
    This commit adds new test cases adapted from the qiskit-tutorials
    repository's circuit basics tutorial:
    
    https://github.com/Qiskit/qiskit-tutorials/blob/stable/0.33.x/tutorials/circuits/01_circuit_basics.ipynb
    
    At the same time the default behavior of parallelism in the test suite
    is changed from class level to method level. This means when running
    against a real backend behind a web services api (like the ibm
    provider). The tests will submit jobs in parallel to improve throughput.

---
+++ qiskit_neko/tests/circuits/test_circuit_basics.py
@@ -0,0 +1,64 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Tests from circuit basics tutorial."""
+
+import math
+
+import ddt
+import numpy as np
+from qiskit import QuantumCircuit, transpile
+from qiskit.quantum_info import Statevector
+
+from qiskit_neko.tests import base
+
+
+@ddt.ddt
+class TestCircuitBasics(base.BaseTestCase):
+    """Tests adapted from circuit basics tutorial."""
+
+    def setUp(self):
+        super().setUp()
+        self.circ = QuantumCircuit(3)
+        self.circ.h(0)
+        self.circ.cx(0, 1)
+        self.circ.cx(0, 2)
+
+
+    @ddt.data(0, 1, 2, 3)
+    def test_ghz_circuit(self, opt_level):
+        """Test execution of ghz circuit."""
+        self.circ.measure_all()
+        tqc = transpile(self.circ, self.backend, optimization_level=opt_level)
+        run_kwargs = {}
+        expected_value = None
+        if hasattr(self.backend.options, 'shots'):
+            run_kwargs['shots'] = 1000
+            expected_value = 500
+        if hasattr(self.backend.options, 'seed_simulator'):
+            run_kwargs['seed_simulator'] = 42
+        job = self.backend.run(tqc, **run_kwargs)
+        result = job.result()
+        counts = result.get_counts()
+        if expected_value is None:
+            expected_value = sum(counts.values()) / 2
+        expected = {'000': expected_value, '111': expected_value}
+        delta = 10**math.floor(math.log10(expected_value))
+        self.assertDictAlmostEqual(counts, expected, delta=delta)
+
+    def test_ghz_circuit_quantum_info(self):
+        """Test statevector simulation with quantum info of ghz circuit."""
+        state = Statevector.from_int(0, 2**3)
+        state = state.evolve(self.circ)
+        expected = np.array([1/math.sqrt(2), 0, 0, 0, 0, 0, 0, 1/math.sqrt(2)],
+                            dtype=complex)
+        np.testing.assert_array_almost_equal(state, expected)
