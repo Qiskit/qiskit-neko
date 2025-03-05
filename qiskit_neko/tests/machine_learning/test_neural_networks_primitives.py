Fix seed and adjust precision for 'reference' primitives tests

qiskit_neko/tests/machine_learning/test_neural_networks_primitives.py
@@ -41,13 +41,13 @@ class TestNeuralNetworksOnPrimitives(base.BaseTestCase):
         self.circuit = QuantumCircuit(1)
         self.circuit.ry(self.input_params[0], 0)
         self.circuit.rx(self.weight_params[0], 0)
-        self.samplers = dict(reference=ReferenceSampler(), aer=AerSampler(run_options={"seed": 42}))
+        self.samplers = dict(reference=ReferenceSampler(seed=42), aer=AerSampler(run_options={"seed": 42}))
         self.estimators = dict(
-            reference=ReferenceEstimator(), aer=AerEstimator(run_options={"seed": 42})
+            reference=ReferenceEstimator(seed=42), aer=AerEstimator(run_options={"seed": 42})
         )
 
     @decorators.component_attr("terra", "aer", "machine_learning")
-    @data(["reference", 4], ["aer", 1])
+    @data(["reference", 2], ["aer", 1])
     @unpack
     def test_sampler_qnn(self, implementation, decimal):
         """Test the execution of quantum neural networks using SamplerQNN."""
@@ -69,7 +69,7 @@ class TestNeuralNetworksOnPrimitives(base.BaseTestCase):
         np.testing.assert_array_almost_equal(weight_grad, [[[-0.2273], [0.2273]]], decimal)
 
     @decorators.component_attr("terra", "aer", "machine_learning")
-    @data(["reference", 4], ["aer", 1])
+    @data(["reference", 2], ["aer", 1])
     @unpack
     def test_estimator_qnn(self, implementation, decimal):
         """Test the execution of quantum neural networks using EstimatorQNN."""