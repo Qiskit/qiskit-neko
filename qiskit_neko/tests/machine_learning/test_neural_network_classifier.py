f0b27a28bad6dafcea27a5b628eddaf1eb98d8e1
Elena Peña Tapia <epenatap@gmail.com>
2025-03-05 15:51:58 +0100

    Remove qiskit_algorithms imports

--- qiskit_neko/tests/machine_learning/test_neural_network_classifier.py
+++ qiskit_neko/tests/machine_learning/test_neural_network_classifier.py
@@ -17,8 +17,8 @@ from ddt import ddt, data
 from qiskit.primitives import StatevectorSampler as ReferenceSampler
 from qiskit_aer.primitives import Sampler as AerSampler
 
-from qiskit_algorithms.optimizers import COBYLA
-from qiskit_algorithms.utils import algorithm_globals
+from qiskit_machine_learning.optimizers import COBYLA
+from qiskit_machine_learning.utils import algorithm_globals
 
 from qiskit_machine_learning.algorithms.classifiers import VQC
 
